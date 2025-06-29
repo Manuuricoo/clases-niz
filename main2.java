6c65e7b8af9e9bd6c6f24f50a26f3fb3afe4a356
Manuuricoo <manuelrico274@gmail.com>
2025-06-29 20:59:13 -0300

    agrego mas tarea

---
+++ main2.java
@@ -0,0 +1,12 @@
+public class main2 {
+    public static void main(String[] args) {
+
+        Alumno alumno1 = new Alumno("Clara", 16, 7.5);
+
+        alumno1.mostrarPromedio();
+
+        alumno1.aumentarPromedio(1);
+
+        alumno1.mostrarPromedio();
+    }
+}
